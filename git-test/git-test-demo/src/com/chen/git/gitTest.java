4565e5eb6ed9b6c62fce1700a3892ceb265269a6
chen <chen@163.com>
2018-12-16 19:03:31 +0800

    添加test方法

--- git-test/git-test-demo/src/com/chen/git/gitTest.java
+++ git-test/git-test-demo/src/com/chen/git/gitTest.java
@@ -8,4 +8,9 @@ public class gitTest {
         System.out.println("hello y");
         System.out.println("hello n");
     }
+    public void test1(){
+        int i=1;
+        i+=100;
+        System.out.println(i);
+    }
 }
