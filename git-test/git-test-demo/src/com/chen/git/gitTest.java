79cf6178dc7c1f48f5c8de6f245956c0da58c4b0
chen <chen@163.com>
2018-12-16 18:54:31 +0800

    将IDE工程项目添加到本地仓库

---
+++ git-test/git-test-demo/src/com/chen/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.chen.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello w");
+        System.out.println("hello y");
+        System.out.println("hello n");
+    }
+}
